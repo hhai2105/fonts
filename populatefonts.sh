e696fa48d834fa424ce096a8f0cba2c5f4cf6b83
Hai Nguyen <hhai2105@gmail.com>
2022-05-19 16:56:24 -0400

    create a fonts management repository

---
+++ populatefonts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+cp -r ./usr/local/share/fonts/* /usr/local/share/fonts/
+cp -r ./.local/share/fonts/*  ${HOME}/.local/share/fonts/
+
---
+++ pullfonts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+cp -r /usr/local/share/fonts/ ./usr/local/share/fonts/
+cp -r ${HOME}/.local/share/fonts/ ./.local/share/fonts/
+
