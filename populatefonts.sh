88a2287bbad136b64af86eb1c5a1f6b26badf83c
Hai Nguyen <hhai2105@gmail.com>
2022-05-19 17:46:15 -0400

    update populate fonts scripts

--- populatefonts.sh
+++ populatefonts.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env zsh
 
+mkdir -p /usr/local/share/fonts
+mkdir -p ${HOME}/.local/share/fonts
+
 cp -r ./usr/local/share/fonts/* /usr/local/share/fonts/
 cp -r ./.local/share/fonts/*  ${HOME}/.local/share/fonts/
 
+fc-cache -f -v
